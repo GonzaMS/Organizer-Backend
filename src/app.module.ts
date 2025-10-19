ba37634925bc97012f77492b435eab0a111ecfe5
Cristhian Marecos <cristhianmareccos2222@gmail.com>
2025-10-19 18:10:12 -0300

    feat(Database Integration): Integrating postgresql - typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from './common/common.module';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfiguration } from './common/config/env.config';
 import { JoiValidationSchema } from './common/config/joi.validation';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -12,6 +13,18 @@ import { JoiValidationSchema } from './common/config/joi.validation';
       load: [EnvConfiguration],
       validationSchema: JoiValidationSchema,
     }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRESQL_HOST,
+      port: Number(process.env.POSTGRESQL_PORT),
+      username: process.env.POSTGRESQL_USER,
+      database: process.env.POSTGRESQL_DB,
+      password: process.env.POSTGRESQL_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
     TeachersModule,
     SeedModule,
     CommonModule,
